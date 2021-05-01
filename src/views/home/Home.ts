66ac90faff8a9a0e1ea97ca1048a5cf33ec6ce33
Kolynes Chinedu <kolyneschinedu@gmail.com>
2021-05-01 12:35:18 +0100

    removed store and redundant views

--- src/views/home/Home.ts
+++ src/views/home/Home.ts
@@ -3,12 +3,16 @@ import {Vue, Component, Ref} from "vue-property-decorator";
 @Component
 export default class Home extends Vue {
     contracts: string[] = [];
+    proposals: IProposal[] = [];
     selectedContract = "";
     showCreateContract = false;
+    creatingContract = false;
     owner1 = "";
     owner2 = "";
     owner3 = "";
     minimumVotes = 2;
+    amount = 0;
+    recipient = "";
 
     @Ref()
     createContractForm!: {validate: () => boolean, reset: () => void};
@@ -18,6 +22,18 @@ export default class Home extends Vue {
     }
 
     async createContract(): Promise<boolean> {
+        return true;
+    }
+
+    async createProposal(): Promise<boolean> {
+        return true;
+    }
+
+    async getProposals(): Promise<void> {
+        return;
+    }
+
+    async voteProposal(proposal: IProposal): Promise<boolean> {
         return true;
     }
 }
